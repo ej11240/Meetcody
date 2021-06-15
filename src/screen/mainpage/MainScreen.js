9cbbdcd960eb0bc065a8d953df764b2f10a77c15
ej11240 <hwangej1124@gmail.com>
2021-06-15 17:12:00 +0900

    feat:서버에서 중간 지점 근처 역  데이터 부분 post 완성 axios로 서버에서 데이터 수신하는 부분 구현 완료

--- src/screen/mainpage/MainScreen.js
+++ src/screen/mainpage/MainScreen.js
@@ -1,43 +1,71 @@
 import * as React from 'react';
 import { useContext } from 'react';
 import AppContext from '../../context/AppContext';
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import   ActionBar  from 'react-native-action-bar';
+import ActionBar from 'react-native-action-bar';
+import axios from 'axios';
+import { set } from 'lodash';
+
 
 export default function MainScreen({ navigation }) {
   const myContext = useContext(AppContext);
-    return (
-  <SafeAreaView>
-      <View style={{ flex: 1, alignItems: 'center' }}>
-        <ActionBar
-        titleContainerStyle={{tintColor:"000000"}}
-              containerStyle={{height:50,alignSelf: 'center',paddingRight:20, paddingLeft:20}}
-              backgroundColor={'#fff'}
-              title={'Home'}
-              titleStyle={{color:"#000", alignItems:"center", textAlign:"center"}}
-              onLeftPress={()=>navigation.openDrawer()}
-              leftIconContainerStyle={{marginTop:22}}
-              rightIconContainerStyle={{marginTop:22}}
-              leftIconName={'menu'}
-              rightIcons={
-                [
-                  {
-                    
-                    image: require('../../asset/searchicon.png'),
-                    onPress: () => console.log('Right Phone !'),
-                    badge:'1',
-                    width:40
-                  }
-                ]
-              }
-              rightIconImageStyle={{tintColor: '#000000', width:10}}
-              leftIconImageStyle={{tintColor: '#000000'}}
-              disableShadows={true}
-        />
-        <Text>Home</Text>
-      </View>
-      </SafeAreaView>
-    );
-  }
+  const [stations,setStations]=React.useState(null);
   
+
+  axios.post('/start_input', {
+    "input1": "Sookmyung Women's University",
+    "input2": "Chung-Ang University"
+  })
+    .then(response => { console.log("input success!!!!") })
+    .catch(response => { console.log("falied") });
+
+  axios.get('http://localhost:8080/subway')
+    .then(response => { 
+      // setStations(response.data);
+      const station=response.data;
+      console.log(station);
+    }) // SUCCESS
+    .catch(response => { console.log(response); }); // ERROR
+
+    
+  return (
+
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <ActionBar
+        containerStyle={{ height: 100, alignSelf: 'center', paddingRight: 20, paddingLeft: 20 }}
+        backgroundColor={'#fff'}
+        title={'Home'}
+        titleStyle={{ color: "#000", alignItems: "center", textAlign: "center" }}
+        onLeftPress={() => navigation.openDrawer()}
+        leftIconContainerStyle={{ marginTop: 22 }}
+        rightIconContainerStyle={{ marginTop: 22 }}
+        leftIconName={'menu'}
+        rightIcons={
+          [
+            {
+
+              image: require('../../asset/searchicon.png'),
+              onPress: () => console.log('Right Phone !'),
+              badge: '1',
+              width: 40
+            }
+          ]
+        }
+        rightIconImageStyle={{ tintColor: '#000000', width: 10 }}
+        leftIconImageStyle={{ tintColor: '#000000' }}
+        disableShadows={true}
+      />
+      <View style={{ alignItems:"flex-start"}}>
+      <Text style={{ textAlign:"left"}}>좋은 아침이예요!{"\n"}
+        오늘은 6월 15일 화요일 입니다!,.
+
+
+      </Text>
+      
+      
+      </View>
+    </View>
+
+  );
+}
