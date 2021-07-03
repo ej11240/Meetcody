6d088324609e15ae6fa6ae3385131239a5cf4cc9
beanxx <tnqls991211@naver.com>
2021-07-03 17:47:43 +0900

    feat: CreateMeetScreen-Selectbox 추가

--- src/screen/createpage/CreateMeetScreen.js
+++ src/screen/createpage/CreateMeetScreen.js
@@ -1,8 +1,9 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable no-unused-vars */
 import * as React from 'react';
 import {useContext} from 'react';
-import {useState} from 'react';
+import {Component, useState, useEffect} from 'react';
 import AppContext from '../../context/AppContext';
 import {
   View,
@@ -18,15 +19,70 @@ import styles from './styles';
 import {TextInput} from 'react-native-paper';
 import ActionBar from 'react-native-action-bar';
 import MultiSelect from 'react-native-multiple-select';
+
 //import {CalendarList} from 'react-native-common-date-picker';
 // import {Modal} from 'react-native-modals';
 // npm install react-native-simple-time-picker --save
 // npm install react-native-picker-select
 
+const LikeClock = () => {
+  const items = [
+    {
+      id: 'dawn',
+      name: '새벽',
+    },
+    {
+      id: 'morning',
+      name: '아침',
+    },
+    {
+      id: 'noon',
+      name: '점심',
+    },
+    {
+      id: 'night',
+      name: '저녁',
+    },
+  ];
+
+  // Data Source for the SearchableDropdown
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const onSelectedItemsChange = selectedItems => {
+    // Set Selected Items
+    setSelectedItems(selectedItems);
+  };
+
+  return (
+    <View>
+      <MultiSelect
+        hideTags
+        items={items}
+        uniqueKey="id"
+        onSelectedItemsChange={onSelectedItemsChange}
+        selectedItems={selectedItems}
+        selectText="선호하는 시간대를 선택해주세요."
+        searchInputPlaceholderText="선호하는 시간대를 선택해주세요."
+        onChangeInput={text => console.log(text)}
+        tagRemoveIconColor="#000"
+        tagBorderColor="#CCC"
+        tagTextColor="#CCC"
+        selectedItemTextColor="#CCC"
+        selectedItemIconColor="#CCC"
+        itemTextColor="#000"
+        displayKey="name"
+        searchInputStyle={{color: '#CCC'}}
+        submitButtonColor="#000"
+        submitButtonText="확인"
+      />
+      {/* <View>{this.multiSelect.getSelectedItemsExt(selectedItems)}</View> */}
+    </View>
+  );
+};
+
 export default function CreateMeetScreen({navigation}) {
   const myContext = useContext(AppContext);
   const [text, setText] = React.useState('');
-  const [modalVisible, setModalVisible] = useState(false);
   const androidBool = Platform.OS === 'android' ? true : false;
 
   return (
@@ -41,7 +97,7 @@ export default function CreateMeetScreen({navigation}) {
         backgroundColor={'#fff'}
         title={'약속 생성'}
         titleStyle={{color: '#000', textAlign: 'center'}}
-        onLeftPress={() => navigation.navigate('MainScreen')}
+        onLeftPress={() => navigation.goBack()}
         leftIconContainerStyle={{marginTop: 20}}
         leftIconName={'back'}
         leftIconImageStyle={{tintColor: '#000000'}}
@@ -54,46 +110,18 @@ export default function CreateMeetScreen({navigation}) {
           value={myContext.meeting}
           onChangeText={text => setText(text)}
         />
-
         <Text style={{paddingTop: 20}}>약속 날짜 범위</Text>
-
         <Text>회의 지속 시간</Text>
-
         <Text>선호 시간대(중복 선택 가능)</Text>
-        {/* <MultiSelect
-          hideTags
-          items={items}
-          uniqueKey="id"
-          ref={component => {
-            this.multiSelect = component;
-          }}
-          onSelectedItemsChange={this.onSelectedItemsChange}
-          selectedItems={selectedItems}
-          selectText="Pick Items"
-          searchInputPlaceholderText="Search Items..."
-          onChangeInput={text => console.log(text)}
-          altFontFamily="ProximaNova-Light"
-          tagRemoveIconColor="#CCC"
-          tagBorderColor="#CCC"
-          tagTextColor="#CCC"
-          selectedItemTextColor="#CCC"
-          selectedItemIconColor="#CCC"
-          itemTextColor="#000"
-          displayKey="name"
-          searchInputStyle={{color: '#CCC'}}
-          submitButtonColor="#CCC"
-          submitButtonText="Submit"
-        />
-        <View>{this.multiSelect.getSelectedItemsExt(selectedItems)}</View> */}
+        <LikeClock />
 
         <Text>장소 선택</Text>
-
         <Text>캘린더 접근 허용 기한 선택</Text>
       </ScrollView>
       <View style={styles.fixToText}>
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('MainScreen')}>
+          onPress={() => navigation.goBack()}>
           <Text>취소</Text>
         </TouchableOpacity>
 
