596c55ff6afa4c43d366699befb348c2c5fdb39d
ej11240 <hwangej1124@gmail.com>
2021-08-24 22:43:16 +0900

    fix: git 병합 충돌 해결 깃 병합 충돌 해결

---
+++ src/screen/inviteacceptpage/InviteAcceptScreen.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {useContext} from 'react';
+import AppContext from '../../context/AppContext';
+import {Button, View, Text, SafeAreaView} from 'react-native';
+import ActionBar from 'react-native-action-bar';
+import styles from './styles';
+import MainActionBar from '../mainpage/MainActionBar';
+
+export default function FriendsListScreen({navigation}) {
+    
+}
